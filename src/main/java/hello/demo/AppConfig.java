1a7b7519577b18e6d8e7f033132ed1ccdfffc59c
gyuhoon.kim <closet0426@gmail.com>
2024-01-09 23:48:28 +0900

    refact: AppConfig 구체 클래스 생성 시 메소드 사용

--- src/main/java/hello/demo/AppConfig.java
+++ src/main/java/hello/demo/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.demo;
 
+import hello.demo.discount.DiscountPolicy;
 import hello.demo.discount.FixDiscountPolicy;
+import hello.demo.discount.RateDiscountPolicy;
+import hello.demo.member.MemberRepository;
 import hello.demo.member.MemberService;
 import hello.demo.member.MemberServiceImpl;
 import hello.demo.member.MemoryMemberRepository;
@@ -13,13 +16,24 @@ public class AppConfig {
     - 이러한 방식을 생성자를 통해서 주입(injection)한다고 말한다
     - 객체의 생성과 연결을 담당한다
     - 클라이언트(memberServiceImpl) 입장에서는 의존 관계를 외부에서 주입된 것으로 보이기 때문에 DI(의존관계 주입)이라고 부른다
+    - AppConfig의 역할: 구성 영역
      */
+    private MemberRepository memberRepository() { // 인터페이스를 반환해주는 method
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 비율 할인 정책으로 수정
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository()); // cmd + opt + m (리팩토링)
 
     }
-
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+
 }
